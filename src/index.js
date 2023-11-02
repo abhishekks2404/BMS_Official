cf93047377b77b41ae2744b4ee1fecc608a7fc13
abhishekks2404 <abhishek.singh@blenheimchalcot.com>
2023-11-02 20:40:36 +0530

    third

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '/Users/abhishek_admin/Desktop/BC_APPLICATION/bc-app/src/index.css';
+import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 
